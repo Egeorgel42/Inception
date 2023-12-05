fe60e59199897e6cc287a783666a0f25e4971092
letnys <letnys@inception.myguest.virtualbox.org>
2023-12-05 14:45:11 +0100

    almost done

--- srcs/requirements/mariadb/tools/startscript.sh
+++ srcs/requirements/mariadb/tools/startscript.sh
@@ -1,8 +1,44 @@
-service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+#!/bin/bash
+
+mysql_secure_install()
+{
+	mysql -uroot<<EOF
+	UPDATE mysql.user SET Password = PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User = 'root';
+	DROP DATABASE IF EXISTS test;
+	DELETE FROM mysql.user WHERE User='';
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	FLUSH PRIVILEGES;
+EOF
+}
+
+create_database()
+{
+	mysql -uroot<<EOF
+	CREATE DATABASE $MYSQL_DATABASE;
+	CREATE USER IF NOT EXISTS $MYSQL_USER IDENTIFIED BY '$MYSQL_PASSWORD';
+	GRANT ALL ON $MYSQL_DATABASE.* TO $MYSQL_USER; FLUSH PRIVILEGES;
+	QUIT
+EOF
+}
+
+main()
+{
+	echo Starting mysql service...
+	if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+		service mysql start
+		echo Mysql_secure_install
+		mysql_secure_install
+		echo secure install done.
+		echo Create database...
+		create_database
+		sleep 2
+		echo Database created
+		service mysql stop
+	else
+		echo Database exist.
+	fi
+	echo Execute mysqld
+	exec mysqld_safe
+}
+
+main
--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,22 +1,30 @@
-sleep 10
+sleep 5
 if [ ! -f /var/www/wordpress/wp-config.php ]
 then
+	sed -i '/;clear_env = no/ { c \
+	clear_env = no
+	}' /etc/php/7.3/fpm/pool.d/www.conf
+	sed -i '/listen =/ { c \
+	listen=wordpress:9000
+	}' /etc/php/7.3/fpm/pool.d/www.conf
+	mkdir -p /var/www/wordpress
+	cd /var/www/wordpress
+	wp core download --allow-root
 	wp config create --allow-root \
 		--dbname=$MYSQL_DATABASE \
 		--dbuser=$MYSQL_USER \
 		--dbpass=$MYSQL_PASSWORD \
-		--dbhost=mariadb:3306 \
+		--dbhost=mariadb \
 		--path='/var/www/wordpress'
 	wp core install --allow-root \
 		--url='https://egeorgel.42.fr' \
 		--title='Inception' \
-		--admin_user='admin' \
-		--admin_password='admin' \
-		--admin_email='egeorgel@student.42nice.fr' \
-		--path='/var/www/wordpress'
+		--admin_user=$WP_USER \
+		--admin_password=$WP_PASSWORD \
+		--admin_email='egeorgel@student.42nice.fr'
 	wp user create egeorgel --allow-root \
-	egeorgel@student.42nice.fr \
-	--user_pass=pass \
-	--path='/var/www/wordpress'
+	ethangeorgel2003@gmail.com \
+	--user_pass=pass
 fi
+echo lauching php
 /usr/sbin/php-fpm7.3 -F
